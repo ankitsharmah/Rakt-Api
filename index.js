0cd0f2ba84574a7432d91f7fb94194f257add6d9
Ankit-Sharma <asrover9810@gmail.com>
2025-01-09 15:39:44 +0530

    index.js intigrated with routes

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ import dotenv from 'dotenv';
 import { app, server } from './socket/socket.js';
 import connectDb from './config/db.js';
 import AuthRoutes from "./routes/AuthRoutes.js"
-import bloodRoutes from "./routes/bloodRoutes.js"
+import bloodRoutes from "./routes/bloodRoutes.js" 
 import bloodBankRoutes from "./routes/bloodBankRoutes.js"
+import requestsRoute from "./routes/requestsRoute.js"
+
 dotenv.config();
 
 // Middleware to parse JSON bodies
@@ -28,6 +30,8 @@ app.use(cors(corsOptions));
 app.use("/api/v1/auth",AuthRoutes)
 app.use("/api/v1/blood",bloodRoutes)
 app.use("/api/v1/blood-bank",bloodBankRoutes)
+app.use("/api/v1/requets",requestsRoute)
+
 
 app.get("/",(req,res)=>{
     return res.status(200).json({
