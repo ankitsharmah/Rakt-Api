998c2f8607b97372b00076860889517d0321d4aa
Ankit-Sharma <asrover9810@gmail.com>
2025-01-09 15:39:07 +0530

    bllood bank route made and tested

--- routes/bloodBankRoutes.js
+++ routes/bloodBankRoutes.js
@@ -1,8 +1,8 @@
 import express from "express"
-import { saveBloodBank } from "../contorller/bloodBankController.js"
+import { allBloodBank, saveBloodBank } from "../contorller/bloodBankController.js"
 
 const router = express.Router()
 
 router.post("/add-bank",saveBloodBank)
-
+router.get("/banks",allBloodBank)
 export default router
